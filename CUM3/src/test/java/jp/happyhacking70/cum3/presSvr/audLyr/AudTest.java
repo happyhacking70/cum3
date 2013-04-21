ba335be35ac126ed788e5b4caafbecbd9e5b42d5
Takeshi Yoshida <happyhacking70@gmail.com>
2013-04-22 07:01:47 +0900

    Aud JUnitized

---
+++ CUM3/src/test/java/jp/happyhacking70/cum3/presSvr/audLyr/AudTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package jp.happyhacking70.cum3.presSvr.audLyr;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import jp.happyhacking70.cum3.cmd.impl.NtfyCmdRegChnl;
+import jp.happyhacking70.cum3.comLyr.DummySender;
+import jp.happyhacking70.cum3.presSvr.seshLyr.DummyAcptAudDisconned;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author happyhacking70@gmail.com
+ * 
+ */
+public class AudTest {
+	static final String audName = "testAudience";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Aud Test");
+	}
+
+	/**
+	 * Test method for
+	 * {@link jp.happyhacking70.cum3.presSvr.audLyr.Aud#getAudName()}.
+	 */
+	@Test
+	public void testGetAudName() {
+		DummySender sender = new DummySender();
+		DummyAcptAudDisconned acpter = new DummyAcptAudDisconned();
+		Aud aud = new Aud(audName, sender, acpter);
+
+		assertEquals(audName, aud.getAudName());
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link jp.happyhacking70.cum3.presSvr.audLyr.Aud#sendCmd(jp.happyhacking70.cum3.cmd.CmdAbst)}
+	 * .
+	 */
+	@Test
+	public void testSendCmdOk() {
+		DummySender sender = new DummySender();
+		DummyAcptAudDisconned acpter = new DummyAcptAudDisconned();
+		Aud aud = new Aud(audName, sender, acpter);
+
+		NtfyCmdRegChnl cmd = new NtfyCmdRegChnl("testSession", "testChnl");
+		aud.sendCmd(cmd);
+		assertNull(acpter.getAudName());
+		assertEquals(cmd, sender.pollCmd());
+		assertNull(sender.pollCmd());
+	}
+
+	/**
+	 * Test method for
+	 * {@link jp.happyhacking70.cum3.presSvr.audLyr.Aud#sendCmd(jp.happyhacking70.cum3.cmd.CmdAbst)}
+	 * .
+	 */
+	@Test
+	public void testSendCmd_COMERR() {
+		DummySender sender = new DummySender();
+		DummyAcptAudDisconned acpter = new DummyAcptAudDisconned();
+		Aud aud = new Aud(audName, sender, acpter);
+
+		sender.emulateComErr();
+
+		NtfyCmdRegChnl cmd = new NtfyCmdRegChnl("testSession", "testChnl");
+		aud.sendCmd(cmd);
+		assertNull(sender.pollCmd());
+		assertEquals(audName, acpter.getAudName());
+	}
+}
