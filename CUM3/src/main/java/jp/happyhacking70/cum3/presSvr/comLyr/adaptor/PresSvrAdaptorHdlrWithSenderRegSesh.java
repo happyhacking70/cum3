3fb07bb501a84a48d5116c2a458520cd5df2d229
Takeshi Yoshida <happyhacking70@gmail.com>
2013-04-27 18:25:48 +0900

    adaptor for ReqCmdRegSesh

---
+++ CUM3/src/main/java/jp/happyhacking70/cum3/presSvr/comLyr/adaptor/PresSvrAdaptorHdlrWithSenderRegSesh.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package jp.happyhacking70.cum3.presSvr.comLyr.adaptor;
+
+import jp.happyhacking70.cum3.cmd.CmdAbst;
+import jp.happyhacking70.cum3.cmd.impl.req.ReqCmdRegSesh;
+import jp.happyhacking70.cum3.cmd.impl.res.ResCmdRegSesh;
+import jp.happyhacking70.cum3.excp.impl.CumExcpIllegalCmdDoc;
+import jp.happyhacking70.cum3.excp.impl.CumExcpIllegalCmdXML;
+import jp.happyhacking70.cum3.excp.impl.seshChnlAudLyr.CumExcpSeshExists;
+import jp.happyhacking70.cum3.presSvr.comLyr.CmdSenderIntf;
+import jp.happyhacking70.cum3.presSvr.seshLyr.SeshMgrPresSvrAllIntf;
+
+/**
+ * @author happyhacking70@gmail.com
+ * 
+ */
+public class PresSvrAdaptorHdlrWithSenderRegSesh implements
+		PresSvrAdaptorHdlrWithSenderIntf {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see jp.happyhacking70.cum3.presSvr.comLyr.adaptor.
+	 * PresSvrAdaptorHdlrWithSenderIntf
+	 * #hndlCmd(jp.happyhacking70.cum3.cmd.CmdAbst,
+	 * jp.happyhacking70.cum3.presSvr.comLyr.CmdSenderIntf,
+	 * jp.happyhacking70.cum3.presSvr.seshLyr.SeshMgrPresSvrAllIntf)
+	 */
+	@Override
+	public CmdAbst hndlCmd(CmdAbst cmd, CmdSenderIntf sender,
+			SeshMgrPresSvrAllIntf seshMgr) throws CumExcpIllegalCmdXML,
+			CumExcpIllegalCmdDoc {
+
+		ReqCmdRegSesh reqCmd = (ReqCmdRegSesh) cmd;
+		ResCmdRegSesh resCmd = null;
+
+		try {
+			seshMgr.regSesh(reqCmd.getSeshName(), sender);
+			sender.sendCmd(new ResCmdRegSesh(reqCmd.getSeshName(),
+					ResCmdRegSesh.RsltTypes.Reged));
+		} catch (CumExcpSeshExists e) {
+			resCmd = new ResCmdRegSesh(reqCmd.getSeshName(),
+					ResCmdRegSesh.RsltTypes.Exists);
+		}
+
+		return resCmd;
+	}
+}
---
+++ CUM3/src/test/java/jp/happyhacking70/cum3/presSvr/comLyr/adaptor/PresSvrAdaptorHdlrWithSenderRegSeshTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package jp.happyhacking70.cum3.presSvr.comLyr.adaptor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.IOException;
+
+import jp.happyhacking70.cum3.cmd.impl.req.ReqCmdRegSesh;
+import jp.happyhacking70.cum3.cmd.impl.res.ResCmdRegSesh;
+import jp.happyhacking70.cum3.excp.impl.CumExcpIllegalCmdDoc;
+import jp.happyhacking70.cum3.excp.impl.CumExcpIllegalCmdXML;
+import jp.happyhacking70.cum3.excp.impl.seshChnlAudLyr.CumExcpSeshExists;
+import jp.happyhacking70.cum3.presSvr.seshLyr.SeshMgrPresSvrAllIntf;
+import jp.happyhacking70.cum3.presSvr.seshLyr.impl.SeshMgrPresSvr;
+import jp.happyhacking70.cum3.test.CumTestBast;
+
+import org.junit.Test;
+
+/**
+ * @author happyhacking70@gmail.com
+ * 
+ */
+public class PresSvrAdaptorHdlrWithSenderRegSeshTest extends CumTestBast {
+
+	/**
+	 * @throws IOException
+	 */
+	public PresSvrAdaptorHdlrWithSenderRegSeshTest() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Test method for
+	 * {@link jp.happyhacking70.cum3.presSvr.comLyr.adaptor.PresSvrAdaptorHdlrWithSenderRegSesh#hndlCmd(jp.happyhacking70.cum3.cmd.CmdAbst, jp.happyhacking70.cum3.presSvr.comLyr.CmdSenderIntf, jp.happyhacking70.cum3.presSvr.seshLyr.SeshMgrPresSvrAllIntf)}
+	 * .
+	 * 
+	 * @throws CumExcpIllegalCmdDoc
+	 * @throws CumExcpIllegalCmdXML
+	 */
+	@Test
+	public void testHndlCmd() throws CumExcpIllegalCmdXML, CumExcpIllegalCmdDoc {
+		SeshMgrPresSvrAllIntf seshMgr = new SeshMgrPresSvr();
+		PresSvrAdaptorHdlrWithSenderRegSesh hdlr = new PresSvrAdaptorHdlrWithSenderRegSesh();
+
+		ReqCmdRegSesh reqCmd = new ReqCmdRegSesh(seshName);
+		ResCmdRegSesh resCmd = null;
+		resCmd = (ResCmdRegSesh) hdlr.hndlCmd(reqCmd, senderForPrestr, seshMgr);
+
+		assertNull(resCmd);
+
+	}
+
+	@Test
+	public void testHndlCmd_EXISTS() throws CumExcpIllegalCmdXML,
+			CumExcpIllegalCmdDoc, CumExcpSeshExists {
+		SeshMgrPresSvrAllIntf seshMgr = new SeshMgrPresSvr();
+
+		seshMgr.regSesh(seshName, senderForPrestr);
+		PresSvrAdaptorHdlrWithSenderRegSesh hdlr = new PresSvrAdaptorHdlrWithSenderRegSesh();
+
+		ReqCmdRegSesh reqCmd = new ReqCmdRegSesh(seshName);
+		ResCmdRegSesh resCmd = null;
+		resCmd = (ResCmdRegSesh) hdlr.hndlCmd(reqCmd, senderForPrestr, seshMgr);
+
+		assertEquals(ResCmdRegSesh.class, resCmd.getClass());
+		assertEquals(ResCmdRegSesh.RsltTypes.Exists.name(), resCmd.getRslt());
+	}
+}
